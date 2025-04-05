78f34e8b899571613a719acbcede6a4967246621
某D君 <564392818@qq.com>
2025-04-05 14:46:35 +0800

    feat：礼物列表接口联调；实现礼物收藏，礼物喜欢及其取消功能

--- src/pages/gift/gift.vue
+++ src/pages/gift/gift.vue
@@ -5,7 +5,7 @@
       :values="items"
       style-type="text"
       :active-color="activeColor"
-      @clickItem="onClickItem"
+      @clickItem="onClickTab"
     />
   </view>
   <!-- 图片列表 -->
@@ -15,22 +15,37 @@
       :key="item.id"
       class="waterfall-item"
       :cover="cover"
-      @click="onClick"
       :border="false"
       padding="0"
       margin="0"
       :is-shadow="false"
     >
-      <view class="waterfall-item-title">{{ item.title }}</view>
+      <view class="waterfall-item-title">{{ item.name }}</view>
       <template v-slot:actions>
         <view class="card-actions text-weaken">
           <view class="card-actions-item">
-            <image class="avatar" :src="item.avatar" />
+            <image class="avatar" :src="item.creatorAvatar" />
             <text>{{ item.creatorName }}</text>
           </view>
           <view class="card-actions-item">
-            <uni-icons type="heart" size="18" color="#999" /><text>{{ item.likeCount }}</text>
-            <uni-icons type="star" size="20" color="#999" /><text>{{ item.starCount }}</text>
+            <uni-icons
+              v-if="item.isFavoritedByUser"
+              type="heart-filled"
+              size="18"
+              color="#999"
+              @click="onLike(item)"
+            />
+            <uni-icons v-else type="heart" size="18" color="#999" @click="onLike(item)" />
+            <text>{{ item.favoriteCount ?? 0 }}</text>
+            <uni-icons
+              v-if="item.isCollectedByUser"
+              type="star-filled"
+              size="20"
+              color="#999"
+              @click="onCollect(item)"
+            />
+            <uni-icons v-else type="star" size="20" color="#999" @click="onCollect(item)" />
+            <text>{{ item.collectCount ?? 0 }}</text>
           </view>
         </view>
       </template>
@@ -45,66 +60,93 @@
 import { ref, type Ref } from 'vue'
 import { GiftType } from './gift.type'
 import cover from '@/static/images/test.jpg'
-import avatar from '@/static/images/avatar.png'
+import { http } from '@/utils/http'
+import { onReachBottom, onShow } from '@dcloudio/uni-app'
 
 const items = ref(['礼物广场', '我创建的', '我收藏的']) as Ref<string[]>
 const current = ref(GiftType.ALL) as Ref<GiftType>
 const activeColor = '#007aff'
+// 默认每页加载6条数据
+const defaultPageSize = 6
+// 当前总数
+let total = 0
 
-const mockList = [
-  {
-    title: '这是一个带封面和操作栏的卡片示例，此',
-    cover,
-    avatar,
-    creatorName: 'dxy',
-    likeCount: 23,
-    starCount: 11,
-    id: '1',
+// 触底加载
+onReachBottom(async () => {
+  if (displayedList.value.length >= total) {
+    return
+  }
+
+  const res = await getGfitList({
+    start: displayedList.value.length,
+    pageSize: defaultPageSize,
     isMine: false,
-    isStar: false,
-  },
-  {
-    title: '这是我创建的礼物',
-    cover,
-    avatar,
-    creatorName: 'dxy',
-    likeCount: 23,
-    starCount: 11,
-    id: '2',
-    isMine: true,
-    isStar: false,
-  },
-  {
-    title: '这是我收藏的礼物',
-    cover,
-    avatar,
-    creatorName: 'dxy',
-    likeCount: 23,
-    starCount: 11,
-    id: '2',
-    isMine: false,
-    isStar: true,
-  },
-]
-const displayedList = ref(mockList) as Ref<any[]>
+    isCollect: false,
+  })
+  displayedList.value = [...displayedList.value, ...res.rows]
+})
 
-const onClickItem = (e: any) => {
-  if (current.value !== e.currentIndex) {
-    current.value = e.currentIndex
-  }
+onShow(() => {
+  refreshList(current.value)
+})
 
-  if (current.value === GiftType.ALL) {
-    displayedList.value = mockList
-  }
-
-  if (current.value === GiftType.OWN) {
-    displayedList.value = mockList.filter((item) => item.isMine)
-  }
-
-  if (current.value === GiftType.LIKE) {
-    displayedList.value = mockList.filter((item) => item.isStar)
-  }
+const onClickTab = ({ currentIndex }: any) => {
+  refreshList(currentIndex)
 }
+
+/*
+ * 重新获取列表
+ */
+const refreshList = async (currentTab: GiftType) => {
+  let res: any = []
+  if (currentTab === GiftType.ALL) {
+    res = await getGfitList()
+  }
+
+  if (currentTab === GiftType.OWN) {
+    res = await getGfitList({
+      start: 0,
+      pageSize: defaultPageSize,
+      isMine: true,
+      isCollect: false,
+    })
+  }
+
+  if (currentTab === GiftType.LIKE) {
+    res = await getGfitList({
+      start: 0,
+      pageSize: defaultPageSize,
+      isMine: false,
+      isCollect: true,
+    })
+  }
+
+  displayedList.value = res.rows
+  total = res.total
+}
+
+const getGfitList = async (
+  params: {
+    start: number
+    pageSize: number
+    isMine: boolean
+    isCollect: boolean
+  } = { start: 0, pageSize: defaultPageSize, isMine: false, isCollect: false },
+) => {
+  const res: any = await http({
+    url: '/gift/list',
+    method: 'GET',
+    data: {
+      start: params.start,
+      pageSize: params.pageSize,
+      isMine: params.isMine,
+      isCollect: params.isCollect,
+    },
+  })
+
+  return res.data
+}
+const displayedList = ref([]) as Ref<any[]>
 
 const addGift = () => {
   uni.navigateTo({
@@ -112,8 +154,33 @@ const addGift = () => {
   })
 }
 
-const onClick = (e: any) => {
-  console.log(e)
+const onLike = async (item: any) => {
+  await http({
+    url: `/gift/like/${item.id}/${!item.isFavoritedByUser}`,
+    method: 'POST',
+  })
+  item.isFavoritedByUser = !item.isFavoritedByUser
+
+  if (item.isFavoritedByUser) {
+    item.favoriteCount += 1
+  } else {
+    item.favoriteCount -= 1
+  }
+}
+
+const onCollect = async (item: any) => {
+  await http({
+    url: `/gift/collect/${item.id}/${!item.isCollectedByUser}`,
+    method: 'POST',
+  })
+  item.isCollectedByUser = !item.isCollectedByUser
+
+  if (item.isCollectedByUser) {
+    item.collectCount += 1
+  } else {
+    item.collectCount -= 1
+    displayedList.value.splice(displayedList.value.indexOf(item), 1)
+  }
 }
 </script>
 
